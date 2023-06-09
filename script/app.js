a5cc309ff6f85c6f8f4edfedb79dbb686f5764b0
Huzaira Arif <huzziha@gmail##.com>
2023-06-09 19:22:54 +0100

    Initialize Project

---
+++ script/app.js
@@ -0,0 +1,115 @@
+
+                            // Assignement  1
+    
+    // alert("Welcome to our website! We're happy to have you here.");
+    
+
+//     
+    // var password = prompt("Please enter your passowrd ")
+    // if(password!== "correctpassword")
+    //     alert("Error! Please enter a valid password.")
+
+
+    // alert("Welcome to JS land.../nHappy coding!");
+
+
+    // alert("Welcome to JS land...");
+    // alert("Happy coding!");
+
+
+    // alert("Welcome to JS land...");
+    // alert("Happy coding!/nPrevent this page from creating additional dialouges.");
+
+
+    // console.log("HelloI can run js through my web browser.  ");
+    // alert( " Hello I can run js through my web browser." )
+
+
+                             // Assignment 2
+
+   
+// var username;
+
+
+// var myName = "John Doe";
+
+    
+// var message = "Hello, World!";
+//     alert(message);
+
+
+// var firstName = "John";
+// var lastName  = "Doe";
+// var age = 20;
+// var grade = 12;
+// alert("Name: " + firstName + " " + lastName);
+//     alert("Age: " + age);
+//     alert("Grade: " + grade);
+
+    
+// alert(pizza.slice(0,-1));
+// alert(pizza.slice(0,-2));
+// alert(pizza.slice(0,-3));
+// alert(pizza.slice(0,-4));
+
+
+// var email = "example@example.com";
+
+
+// alert("I am trying to learn from the box" + book);
+
+
+// var paragraph =
+//     Document.createEement("p");
+// paragraph.textContent = "Yah! I can write HTML content through JavaScript in browser through JS";
+
+
+                                  // ASSIGNMENT 3
+
+// var age = 15;
+// alert("I am " + age + yearsold);
+
+
+// var visits = 14;
+// document.write("You have visited this site" + visits + "times");
+
+
+// var birthyear = 1990;
+// document.write("My birth year is " + birthyear + ". data-type of my declared variable is " + typeof(birthyear));
+
+
+
+// var visitorName = "John Doe"; 
+// var productTitle = "T-SHIRT";
+// var quantity = 5;
+// dOcument.write(visitorName + "ordered" + quantity + " " + productTitle + "(s)on XYZ Clothing store");
+  
+
+                                //    ASSIGNMENT 5
+
+    // var num1=5;
+    // var num2=7;
+    // var difference=num1 - num2;
+    // console.log("the difference of"+num1 + "and" + num2 + "is" + difference);      
+    
+    // var num1=5;
+    // var num2=7;
+    // var product=num1 * num2;
+    // console.log("the product of"+num1 + "and" + num2 + "is" + product);      
+    
+    // var num1=5;
+    // var num2=7;
+    // var quotient=num1 / num2;
+    // console.log("the quotient of"+num1 + "and" + num2 + "is" + quotient);      
+    
+    // var num1=5;
+    // var num2=7;
+    // var sum=num1 + num2;
+    // console.log("the sum of"+num1 + "and" + num2 + "is" + sum);      
+    
+    // var remainder= num1 % num2
+   
+    // console.log("the remainder of"+num1 + "divided by" + num2 + "is" + remainder);      
+    
+
+    
